64ea9eabaf726a2084dec962fbaff839334674dd
Ubuntu <ubuntu@ip-172-31-44-170.ec2.internal>
2026-06-27 02:33:47 +0000

    them yml

--- api/config/database.php
+++ api/config/database.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'dsn' => 'mysql:unix_socket=/cloudsql/php-app-491210:asia-southeast1:qlsv-db;dbname=quan_ly_sinh_vien;charset=utf8mb4',
+    'dsn' => 'mysql:host=db;dbname=quan_ly_sinh_vien;charset=utf8mb4',
     'username' => 'root', 
-    'password' => 'Hieumaiduc@2005', 
+    'password' => 'rootpassword', 
 ];
